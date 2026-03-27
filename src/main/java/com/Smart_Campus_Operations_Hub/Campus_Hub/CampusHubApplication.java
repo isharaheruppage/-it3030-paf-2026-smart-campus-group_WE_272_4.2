f869228b1d74aa7d6e9df9969a23e03fd3b20f76
H T ISHARA <isharatharushi33@gmail.com>
2026-03-27 15:31:17 +0530

    folder structure created

---
+++ src/main/java/com/Smart_Campus_Operations_Hub/Campus_Hub/CampusHubApplication.java
@@ -0,0 +1,13 @@
+package com.Smart_Campus_Operations_Hub.Campus_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Smart_Campus_Operations_Hub/Campus_Hub/CampusHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart_Campus_Operations_Hub.Campus_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
